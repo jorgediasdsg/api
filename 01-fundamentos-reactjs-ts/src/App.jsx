550defa3df0afde22b00ea58adfb3172381a0ff7
Jorge Dias <jorgediasdsg@gmail.com>
2023-03-13 14:12:44 -0300

    ADD: Finalizado em JS

---
+++ 01-fundamentos-reactjs-ts/src/App.jsx
@@ -0,0 +1,63 @@
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
+
+import './global.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/jorgediasdsg.png',
+      name: 'Jorge Dias',
+      role: 'Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Loren ipsum dolor sit amet consectetur adipisicing elit.' },
+      { type: 'paragraph', content: 'Quaerat, nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+      { type: 'paragraph', content: 'Quaerat, nemo.🤣🤣🤣' },
+      { type: 'image', content: 'https://source.unsplash.com/random/750x250' },
+      { type: 'paragraph', content: 'Quaerat, nemo.' },
+      { type: 'hashtag', content: '#aa #bb #cc' },
+      { type: 'link', content: 'https://www.google.com.br' },
+    ],
+    publishedAt: new Date('2023-03-12 18:50:21'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Another bit the dust.' },
+      { type: 'image', content: 'https://source.unsplash.com/random/650x650' },
+      { type: 'hashtag', content: '#aa #bb #cc' },
+      { type: 'link', content: 'https://www.google.com.br' },
+    ],
+    publishedAt: new Date('2023-03-12 18:50:21'),
+  },
+]
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wapper}>
+        <Sidebar  />
+        <main>
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              post={post} />
+          ))}
+        </main>
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ 01-fundamentos-reactjs-ts/src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { Avatar } from './Avatar';
+import sytle from './Sidebar.module.css';
+
+// import { AddressBook } from 'phosphor-react'
+
+//https://phosphoricons.com/
+
+export function Sidebar() {
+    return (
+        <aside className={sytle.sidebar}>
+            <img
+                className={sytle.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+            />
+            
+            <div className={sytle.profile}>
+                <Avatar src="https://github.com/jorgediasdsg.png" />
+                
+                <strong>Jorge Dias</strong>
+                <span>Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    {/* <AddressBook size={20} /> */}
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
