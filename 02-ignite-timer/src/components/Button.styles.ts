03773fc6599e5413caf09d2e85a1bcd4f45c38d9
Jorge Dias <jorgediasdsg@gmail.com>
2023-03-15 07:45:09 -0300

    ADD: Eslint

--- 02-ignite-timer/src/components/Button.styles.ts
+++ 02-ignite-timer/src/components/Button.styles.ts
@@ -1,17 +1,16 @@
-import style, { css } from 'styled-components';
+import style, { css } from 'styled-components'
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-    variant: ButtonVariant
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green'
-
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
 }
 
 export const ButtonContainer = style.button<ButtonContainerProps>`
@@ -22,13 +21,16 @@ export const ButtonContainer = style.button<ButtonContainerProps>`
     margin: 8px;
 
     
-    background-color: ${props => props.theme['green-500']};
-    color: ${props => props.theme.white};
+    background-color: ${(props) => props.theme['green-500']};
+    color: ${(props) => props.theme.white};
 
 
     // ${({ variant }) => buttonVariants[variant]}
 
-    // ${props => {return css`background-color: ${buttonVariants[props.variant]}`; }}
+    // ${(props) => {
+      return css`
+        background-color: ${buttonVariants[props.variant]};
+      `
+    }}
 
-`;
-
+`
